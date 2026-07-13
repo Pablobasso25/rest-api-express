5808da502094d62079778db971d53f9500bc37fa
Pablo Basso <pablobasso25@gmail.com>
2026-07-12 22:30:07 -0300

    feat: agregar archivo app.js para configurar el servidor y gestionar rutas de películas

---
+++ backend-con-DB-postgresql-prisma/app.js
@@ -0,0 +1,20 @@
+import express, { json } from "express";
+import { createMovieRouter } from "./routes/movies.js";
+import { corsMiddleware } from "./middlewares/cors.js";
+
+export const createApp = ({ movieModel }) => {
+  const app = express();
+  //middleware nativo de express que me permite leer el body que viene junto a la request (envia el usuario en una peticion post por ejempo)
+  app.use(json());
+  app.use(corsMiddleware());
+  app.disable("x-powered-by"); // deshabilitar el header x-powered-by : express
+
+  // Todos los recursos que sean MOVIES se identifican con /movies
+  app.use("/movies", createMovieRouter({ movieModel }));
+
+  const PORT = process.env.PORT ?? 1234;
+
+  app.listen(PORT, () => {
+    console.log(`Server listening on port http://localhost:${PORT}`);
+  });
+};
